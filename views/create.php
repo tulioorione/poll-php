057f82e72a0a0c9b42e9294a3e90bf19f7c8a4bc
Tulio Orione <tulioorione@gmail.com>
2026-05-26 16:32:52 -0300

    Substitui h1 por links de navegação nas views

--- views/create.php
+++ views/create.php
@@ -1,1 +1,4 @@
-<h1>CRIAR ENQUETE</h1>
+<div class="mt-3">
+    <a href="resultados.php" class="btn btn-primary btn-sm">Ver resultados</a>
+    <a href="index.php" class="btn btn-outline-secondary btn-sm">Home</a>
+</div>
--- views/results.php
+++ views/results.php
@@ -1,1 +1,4 @@
-<h1>RESULTADOS DA ENQUETE</h1>
+<div class="mt-3">
+    <a href="criar.php" class="btn btn-primary btn-sm">Criar nova enquete</a>
+    <a href="index.php" class="btn btn-outline-secondary btn-sm">Home</a>
+</div>
