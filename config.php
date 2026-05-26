c93e623ffe337a5f19e7a93de465937439aef4ba
Tulio Orione <tulioorione@gmail.com>
2026-05-26 14:38:20 -0300

    Adiciona arquivo de configuração inicial do banco e cookies

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Configuração DB
+define("DB_DSN", "mysql:host=localhost;dbname=poll;charset=utf8mb4");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
+// Cookies para votação
+define("VOTE_COOKIE-DAYS", 30);
